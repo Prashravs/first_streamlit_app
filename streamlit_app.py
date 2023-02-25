740f5cc770137b9867dac94b2147f02b0512bb02
Prashravs <126302978+Prashravs@users.noreply.github.com>
2023-02-25 07:59:54 +0530

    Update streamlit_app.py
    added sample emoji

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import streamlit
 streamlit.title( 'My Parents New Healthy dinner')
                 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('/N{U+1F963} Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-boiled free-range egg')
